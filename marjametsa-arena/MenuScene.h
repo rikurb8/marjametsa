e1798bea3f604e61124ce132d3449ab1b9795a63
Jeebus <jeebus@Jeebuss-Mac.local>
2014-11-08 06:39:49 -0800

    Added game menu

---
+++ marjametsa-arena/MenuScene.h
@@ -0,0 +1,14 @@
+//
+//  MenuScene.h
+//  marjametsa-arena
+//
+//  Created on 14.10.2014.
+//  Copyright (c) 2014 rb8. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MenuScene : SKScene
+
+@end
+
---
+++ marjametsa-arena/MenuScene.m
@@ -0,0 +1,124 @@
+//
+//  MenuScene.m
+//  marjametsa-arena
+//
+//  Created on 14.10.2014 by Henkka
+//  Copyright (c). All rights reserved.
+//
+
+
+
+#import "MenuScene.h"
+#import "GameScene.h"
+#import "ViewController.h"
+#import "Hero.h"
+//@import CoreMotion;
+#import <UIKit/UIKit.h>
+
+
+#define kUpdateInterval (1.0f / 60.0f) //60fps
+
+@interface MenuScene()
+
+
+@end
+
+
+@implementation MenuScene
+
+
+// Initialize Menu
+-(id)initWithSize:(CGSize)size {
+    
+    if (self = [super initWithSize:size]) {
+        SKSpriteNode *bgImage = [SKSpriteNode spriteNodeWithImageNamed:@"grass_bg"];
+        bgImage.position = CGPointMake(self.size.width/2, self.size.height/2);
+        [self addChild:bgImage];
+
+    }
+ 
+    // Add MenuButton
+    [self addChild: [self ButtonNode:@"Menu"]];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button.frame =CGRectMake(100, 170, 100, 30);
+    [button setTitle:@"Click me" forState:UIControlStateNormal];
+    [self.view addSubview:button];
+    
+    [self addChild: [self ButtonNode:@"Exit"]];
+    UIButton *exitbutton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    exitbutton.frame =CGRectMake(100, 170, 100, 30);
+    [exitbutton setTitle:@"Click me" forState:UIControlStateNormal];
+    [self.view addSubview:exitbutton];
+
+    
+    
+    return self;
+
+}
+
+
+// Menu Button node
+- (SKSpriteNode *)ButtonNode:(NSString *) buttonType
+{
+    if([buttonType isEqualToString:@"Menu"]){
+    SKSpriteNode *MenuNode = [SKSpriteNode spriteNodeWithImageNamed:@"StartButton.png"];
+    MenuNode.position = CGPointMake(300, 150);
+    MenuNode.name = @"startButton";//how the node is identified later
+    
+    
+    return MenuNode;
+    }else if([buttonType isEqualToString:@"Exit"]){
+        SKSpriteNode *ExitNode = [SKSpriteNode spriteNodeWithImageNamed:@"exitButton.png"];
+        ExitNode.position = CGPointMake(300, 80);
+        ExitNode.name = @"exitButton";
+        return ExitNode;
+    
+    }else{
+        SKSpriteNode *ExitNode = [SKSpriteNode spriteNodeWithImageNamed:@"exitButton.png"];
+        ExitNode.position = CGPointMake(300, 80);
+        ExitNode.name = @"exitButton";
+        return ExitNode;
+    }
+    
+}
+
+
+
+
+
+
+//
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    SKNode *node = [self nodeAtPoint:location];
+    
+    //if StartButton touched start the GameScene
+    if ([node.name isEqualToString:@"startButton"]) {
+        NSLog(@"Start Pressed");
+
+        SKView * sView = (SKView *)self.view;
+
+        
+        SKScene * Gscene = [GameScene sceneWithSize:sView.bounds.size];
+        Gscene.scaleMode = SKSceneScaleModeAspectFill;
+        
+        // Present the scene.
+        [sView presentScene:Gscene];
+        
+        
+    }
+    
+    if ([node.name isEqualToString:@"exitButton"]) {
+        exit(0);
+    }
+
+    
+    
+    return;
+}
+@end
+
+
+
